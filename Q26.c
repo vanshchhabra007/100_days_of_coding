6863b6955202a2d87356e3733d760ce9ea023518
vanshchhabra007 <vansh.22567@stu.upes.ac.in>
2025-12-03 11:32:42 +0530

    Add files via upload

---
+++ Q26.c
@@ -0,0 +1,17 @@
+// Q26: Write a program to print numbers from 1 to n.
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    // Input value of n
+    scanf("%d", &n);
+
+    // Print numbers from 1 to n
+    for (i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
