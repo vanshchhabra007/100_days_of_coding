41fa4231ba30bb1c76e407c8fb7428a3e4c7a638
vanshchhabra007 <vansh.22567@stu.upes.ac.in>
2025-12-03 11:26:59 +0530

    Add files via upload

---
+++ Q8.c
@@ -0,0 +1,18 @@
+// Q8: Write a program to find and display the sum of the first n natural numbers.
+
+#include <stdio.h>
+
+int main() {
+    int n, sum;
+
+    // Input n
+    scanf("%d", &n);
+
+    // Calculate sum using the formula
+    sum = n * (n + 1) / 2;
+
+    // Display result
+    printf("Sum=%d", sum);
+
+    return 0;
+}
