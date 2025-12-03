6b54f5dd21f8ff0516d09faef6dba26ece5a2970
vanshchhabra007 <vansh.22567@stu.upes.ac.in>
2025-12-03 11:39:37 +0530

    Add files via upload

---
+++ Q37.c
@@ -0,0 +1,24 @@
+Q37: Write a program to find the LCM of two numbers.
+
+#include <stdio.h>
+
+int main() {
+    int a, b, max, lcm;
+
+    scanf("%d %d", &a, &b);
+
+    max = (a > b) ? a : b;
+
+    while (1) {
+        if (max % a == 0 && max % b == 0) {
+            lcm = max;
+            break;
+        }
+        max++;
+    }
+
+    printf("%d", lcm);
+
+    return 0;
+}
+
