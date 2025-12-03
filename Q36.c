2bd94bac2ab978489bd986c2d8bc87bd55f44a8c
vanshchhabra007 <vansh.22567@stu.upes.ac.in>
2025-12-03 11:39:06 +0530

    Add files via upload

---
+++ Q36.c
@@ -0,0 +1,20 @@
+Q36: Write a program to find the HCF (GCD) of two numbers.
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    while (a != b) {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+
+    printf("%d", a);
+
+    return 0;
+}
